0224ca85fac0bff37b9a2625dcf9d8066963750a
GDinaraG <gubaidullinadinara25@gmail.com>
2026-03-09 22:09:43 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
 export default defineConfig({
-  base: '/Dinaramovies/',
+base: '/Dinaramovies'
   plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
